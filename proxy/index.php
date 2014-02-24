97d71e8bd0655651d71ecf2c16a0d7d5a659b0a4
Rick Buczynski <richard.buczynski@gmail.com>
2014-02-24 04:03:05 -0500

    Added sandbox

--- proxy/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'ChangeOrgApiProxy.php';
-
-ChangeOrgApiProxy::setSecretKey('YOUR_SECRET_KEY');
-
-if(ChangeOrgApiProxy::validate($_GET)) {
-	echo ChangeOrgApiProxy::fetch($_GET);
-}
